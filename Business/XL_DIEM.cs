b2f2e70ce6e5c28511225ebfe05518d680577de5
edrag <edrag@KOIDEV13TH>
2021-08-14 18:43:34 +0700

    Add project files.

---
+++ Business/XL_DIEM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KTLT_Demo.Business
+{
+    public struct DIEM
+    {
+        public int X;
+        public int Y;
+    }
+    public class XL_DIEM
+    {
+        public static double TinhKhoangCach(DIEM a, DIEM b)
+        {
+            return Math.Sqrt((a.X - b.Y) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y));
+        }
+    }
+}
