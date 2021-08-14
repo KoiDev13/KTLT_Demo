ce31a51cf118da932e6a5e62bb9144350528052e
edrag <edrag@KOIDEV13TH>
2021-08-14 18:43:34 +0700

    Add project files.

---
+++ Business/XL_PHANSO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KTLT_Demo.Business
+{
+    public struct PHANSO
+    {
+        public int TuSo;
+        public int MauSo;
+    }
+
+    public class XL_PHANSO
+    {
+       
+        public static PHANSO CongPS(PHANSO a, PHANSO b)
+        {
+            PHANSO kq;
+            kq.TuSo = a.TuSo * b.MauSo + a.MauSo * b.TuSo;
+            kq.MauSo = a.MauSo * b.MauSo;
+            return kq;
+        }
+    }
+}
